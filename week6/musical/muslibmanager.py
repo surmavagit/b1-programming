78841cf5c10bc48b85306065f62ef4625678a39a
surmavagit <georgesurmava@gmail.com>
2025-11-21 16:34:37 +0100

    fix tests, make the input functions

--- week6/musical/muslibmanager.py
+++ week6/musical/muslibmanager.py
@@ -2,7 +2,18 @@ print("Welcome to Music Library Manager!")
 songs = []
 genre_count = {}
 
-song_number=1
-user_song = input(f"Enter Song {song_number}:\n")
+def ask_for_song(song_number):
+    print(f"Enter Song {song_number}:")
+    song_name = input("Song name:")
+    song_genre = input("\nGenre:")
+    song_number = song_number + 1
+    print("")
 
-print(f"Song name: {user_song}")
+def ask_for_input():
+    total_number_of_songs = 5
+    song_number = 1
+    while song_number <= total_number_of_songs:
+        ask_for_song(song_number)
+        song_number = song_number + 1
+
+ask_for_input()
