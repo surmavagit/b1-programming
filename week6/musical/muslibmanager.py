26124cb0517ef4713453e300732d7b27b80e9915
surmavagit <surmava.git@proton.me>
2025-11-22 20:59:42 +0100

    print report on genre statistics

--- week6/musical/muslibmanager.py
+++ week6/musical/muslibmanager.py
@@ -31,6 +31,23 @@ def print_music_library():
         print(f"{i+1}. {song[0]} ({song[1]})")
 
 
+def print_genre_stats():
+    print("=== GENRE STATISTICS ===")
+    most_popular = ('', 0)
+    for g in genre_count:
+        print(f"{g}: {genre_count[g]} song{plural_form(genre_count[g])}")
+        if genre_count[g] > most_popular[1]:
+            most_popular = (g, genre_count[g])
+
+    print(f"Most popular genre: {most_popular[0]}")
+
+
+def plural_form(number):
+    if number != 1:
+        return "s"
+    return ""
+
+
 ask_for_input()
 print_music_library()
-print(genre_count)
+print_genre_stats()
