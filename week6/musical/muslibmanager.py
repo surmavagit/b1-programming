4337ce0e8a393e4af545b2ab28f48ecf305ae52c
surmavagit <georgesurmava@gmail.com>
2025-12-05 13:00:57 +0100

    complete musical library manager

--- week6/musical/muslibmanager.py
+++ week6/musical/muslibmanager.py
@@ -7,6 +7,13 @@ def ask_for_song(song_number):
     song_name = input("Song name:")
     song_genre = input("\nGenre:")
     song_number = song_number + 1
+    songs.append({ 'name': song_name, 'genre': song_genre })
+
+    if song_genre in genre_count:
+        genre_count[song_genre] += 1
+    else:
+        genre_count[song_genre] = 1
+
     print("")
 
 def ask_for_input():
@@ -16,4 +23,20 @@ def ask_for_input():
         ask_for_song(song_number)
         song_number = song_number + 1
 
+def print_report():
+    print("=== YOUR MUSIC LIBRARY ===")
+    for i in range(len(songs)):
+        print(f"{i+1}. {songs[i]['name']} ({songs[i]['genre']})")
+    print("=== GENRE STATISTICS ===")
+    most_popular_genre = [0,"none"]
+    for genre in genre_count:
+        print(f"{genre}: {genre_count[genre]} songs")
+        if genre_count[genre] > most_popular_genre[0]:
+            most_popular_genre[0] = genre_count[genre]
+            most_popular_genre[1] = genre
+
+    print(f"Most popular genre: {most_popular_genre[1]}")
+
+
 ask_for_input()
+print_report()
