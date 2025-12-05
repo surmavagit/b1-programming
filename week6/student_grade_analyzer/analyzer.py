d9ed3f30d3f7b46ddfe0160e51e1630cad6c13cf
surmavagit <georgesurmava@gmail.com>
2025-12-05 16:50:13 +0100

    complete student grade analyzer

--- week6/student_grade_analyzer/analyzer.py
+++ week6/student_grade_analyzer/analyzer.py
@@ -27,7 +27,7 @@ for _, score in student_records:
 
 stats['highest'] = max(scores)
 stats['lowest'] = min(scores)
-stats['average'] = sum(scores)/len(scores)
+stats['average'] = round(sum(scores)/len(scores), 2)
 
 unique_scores = set(scores)
 
@@ -35,4 +35,20 @@ grade_distribution = {}
 for uniscore in unique_scores:
     grade_distribution[uniscore] = scores.count(uniscore)
 
-print(grade_distribution)
+print("=== STUDENT RECORDS ===")
+for i in range(len(student_records)):
+    name, grade = student_records[i]
+    print(f"{i + 1}. {name}: {grade}")
+
+print("=== CLASS STATISTICS ===")
+print(f"Highest Score: {stats['highest']}")
+print(f"Lowest Score: {stats['lowest']}")
+print(f"Average Score: {stats['average']}")
+
+print("=== UNIQUE SCORES ===")
+print(sorted(unique_scores))
+print(f"Total unique scores:", len(unique_scores))
+
+print("=== GRADE DISTRIBUTION ===")
+for grade in sorted(grade_distribution, key=grade_distribution.get, reverse=True):
+    print(f"Score {grade}: {grade_distribution[grade]} students")
